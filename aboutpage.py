941aaa65bdf9af1723170f2dbbb8371a81fadaab
Robert J. Hansen <rob@hansen.engineering>
2022-11-07 15:32:07 -0500

    Cleaned up aboutpage.py.  Looks more Pythonic, a little easier to read.

--- aboutpage.py
+++ aboutpage.py
@@ -1,18 +1,24 @@
-from ban_list_parser import ban_list_parser
+#!/usr/bin/python3
+# coding: UTF-8
 
-from bs4 import BeautifulSoup
-
-import requests
-import re
+"""Pulls down a set of bans and reasons for all systems with which we're
+federated."""
 
 import sys
 import json
-
 from pprint import pprint
 
-class bans( ban_list_parser ):
+import requests
 
-    def __init__( self, config=None ):
+from bs4 import BeautifulSoup
+from ban_list_parser import ban_list_parser
+
+
+class Bans(ban_list_parser):
+    """Pulls down a set of bans and reasons for all systems with which we're
+    federated."""
+
+    def __init__(self, config=None):
         self.config_section = "aboutpage"
         self.ban_prefix = "about_undefined"
         self.config_section_list = "domains"
@@ -20,45 +26,39 @@ class bans( ban_list_parser ):
         if self.config_section in config:
             self.config = config
         else:
-            print( "config.ini [about] is missing - fatal" )
-            sys.exit()
+            print("config.ini [about] is missing — fatal")
+            # rjh -- should fail with an error condition, not an EXIT_SUCCESS.
+            sys.exit(-1)
 
-        pprint( self.config[self.config_section][self.config_section_list] )
-
+        # rjh -- let's make the next few lines a little more legible.
+        thingy = self.config[self.config_section][self.config_section_list]
+        pprint(thingy)
         try:
-            self.domains = json.loads( self.config[self.config_section][self.config_section_list] )
-        except Exception as e:
-            print( "config file section prasers item '{}' does not parse as valid json, please fix - {}".format( self.config_section, e ) )
-            sys.exit()
+            self.domains = json.loads(thingy)
+        except Exception as exc:
+            print(f"config file section parsers item '{self.config_section}'" +
+                  f" doesn't parse as valid json, please fix - {exc}")
+            # rjh -- again, this is not an EXIT_SUCCESS condition
+            sys.exit(-1)
 
     def getbans(self):
         for domain in self.domains:
-            page = requests.get( "https://{}/about/more".format( domain ) )
-            print( page )
-            soup = BeautifulSoup(page.content, 'html5lib')
-
-
-            tables = soup.find_all( 'table' )
-
+            page = requests.get(f"https://{domain}/about/more")
+            print(page)
+            tables = BeautifulSoup(page.content, 'html5lib').find_all('table')
             bans = []
+            accept_set = set(["<th>Server</th>", "<th>Reason</th>"])
 
             for table in tables:
-                ths = table.find_all('th')
-                if all(
-                        str(x) in [
-                            "<th>Server</th>",
-                            "<th>Reason</th>"
-                            ]
-                        for x in ths
-                        ):
-                    for tr in table.find_all('tr'):
-                        if "<th>" in str( tr ):
+                table_headers = [str(X) for X in table.find_all('th')]
+                if all(X in accept_set for X in table_headers):
+                    for tablerow in table.find_all('tr'):
+                        if "<th>" in str(tablerow):
                             continue
-                        cols = tr.find_all('td')
+                        cols = tablerow.find_all('td')
                         cols = [ele.text.strip() for ele in cols]
-                        bans.append( {
-                                "domain": cols[0],
-                                "reason": cols[1]
-                                } )
+                        bans.append({
+                            "domain": cols[0],
+                            "reason": cols[1]
+                        })
             return bans
-
