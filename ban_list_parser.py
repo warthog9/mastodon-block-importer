68221899bc4d8cfd4fb96799cd38a51b2d9cdcca
Robert J. Hansen <rob@hansen.engineering>
2022-11-07 17:28:39 -0500

    More mild formatting fixes.

--- ban_list_parser.py
+++ ban_list_parser.py
@@ -1,6 +1,15 @@
+#!/usr/bin/python3
+# coding: UTF-8
+
+
+"""Parent class for Bans.  Nothing useful is located here."""
+
+
 import sys
 
-class ban_list_parser:
-    def getbans():
+
+class BanListParser:
+    """Parent class for Bans.  Nothing useful is located here."""
+    def getbans(self):
         print("Seriously, you can't use this directly call into a subclass!")
-        sys.exit()
+        sys.exit(-1)
